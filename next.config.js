abd989586d79b809aba048629c835242f12417ba
Thatsaphon <thatphon123@hotmail.com>
2024-03-21 13:39:24 +0700

    fix next config

--- next.config.js
+++ next.config.js
@@ -15,7 +15,7 @@ const nextConfig = {
   },
   experimental: {
     outputFileTracingIncludes: {
-      outputFileTracingRoot: path.join(process.cwd(), 'master-data/*+'),
+      outputFileTracingRoot: path.join(process.cwd(), 'master-data/*'),
     },
   },
 }
