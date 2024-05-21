ace5f6f1aa246e6538e4d48b606aef377cbde23f
Thatsaphon <thatphon123@hotmail.com>
2024-05-21 18:19:58 +0700

    update test create purchase invoice

---
+++ app/purchase/test/barcode-scanned.ts
@@ -0,0 +1,26 @@
+'use server'
+
+import prisma from '@/app/db/db'
+
+export const getSkuByBarcode = async (barcode: string) => {
+    const [result]: {
+        barcode: string
+        name: string
+        detail: string
+        price: number
+        quantityPerUnit: number
+        unit: string
+        partNumber: string
+    }[] = await prisma.$queryRaw`
+            select "GoodsMaster".barcode, "MainSku"."name", "SkuMaster"."detail", "GoodsMaster".price,
+            "GoodsMaster".quantity as "quantityPerUnit", "GoodsMaster".unit, "MainSku"."partNumber" from "MainSku"
+            left join "SkuMaster" on "MainSku"."id" = "SkuMaster"."mainSkuId"
+            left join "GoodsMaster" on "SkuMaster"."id" = "GoodsMaster"."skuMasterId"
+            where "GoodsMaster".barcode = ${barcode}`
+
+    if (!result) throw new Error('Barcode not found')
+    return { ...result }
+
+}
+
+
--- app/purchase/test/page.tsx
+++ app/purchase/test/page.tsx
@@ -18,6 +18,8 @@ import {
 import { Cross1Icon, PlusCircledIcon } from '@radix-ui/react-icons'
 import React from 'react'
 import { searchSku } from './search-sku'
+import { getSkuByBarcode } from './barcode-scanned'
+import toast from 'react-hot-toast'
 
 type Props = {}
 
@@ -35,15 +37,14 @@ export default function TestCreateInvoice({}: Props) {
             quantityPerUnit: number
             quantity: number
             price: number
+            partNumber: string
         }[]
     >([])
+    const [barcodeInput, setBarcodeInput] = React.useState<string>('')
 
     return (
         <div className="p-3">
             <Button variant="ghost">{`< Back`}</Button>
-            {/* <Dialog open={open} onOpenChange={setOpen}>
-                {}
-            </Dialog> */}
 
             <div className="flex flex-col gap-2">
                 <Label className="flex items-center space-x-1">
@@ -71,41 +72,198 @@ export default function TestCreateInvoice({}: Props) {
                     </TableRow>
                 </TableHeader>
                 <TableBody>
+                    {items.map((item, index) => (
+                        <TableRow key={item.barcode}>
+                            <TableCell>{item.barcode}</TableCell>
+                            <TableCell>{item.name}</TableCell>
+                            <TableCell className="text-right">
+                                <Input
+                                    id={`quantity-${index}`}
+                                    className="w-16 text-right"
+                                    type="number"
+                                    value={item.quantity}
+                                    onChange={(e) => {
+                                        const newItems = [...items]
+                                        newItems[index].quantity = Number(
+                                            e.target.value
+                                        )
+                                        setItems(newItems)
+                                    }}
+                                    onKeyDown={(e) => {
+                                        if (e.key === 'Tab') return
+
+                                        const barcodeElement =
+                                            document.getElementById('barcode')
+                                        if (e.key === 'Enter') {
+                                            e.preventDefault()
+                                            barcodeElement?.focus()
+                                        }
+                                        if (e.key === 'ArrowDown') {
+                                            e.preventDefault()
+                                            const nextElement =
+                                                document.getElementById(
+                                                    `quantity-${index + 1}`
+                                                )
+                                            nextElement
+                                                ? nextElement.focus()
+                                                : barcodeElement?.focus()
+                                        }
+                                        if (e.key === 'ArrowUp') {
+                                            e.preventDefault()
+                                            const previousElement =
+                                                document.getElementById(
+                                                    `quantity-${index - 1}`
+                                                )
+                                            previousElement?.focus()
+                                        }
+                                        if (
+                                            e.key === 'ArrowRight' &&
+                                            !e.shiftKey
+                                        ) {
+                                            const priceElement =
+                                                document.getElementById(
+                                                    `price-${index}`
+                                                )
+                                            priceElement?.focus()
+                                        }
+                                    }}
+                                />
+                            </TableCell>
+                            <TableCell className="text-right">
+                                {`${item.unit}(${item.quantityPerUnit})`}
+                            </TableCell>
+                            <TableCell className="text-right">
+                                <Input
+                                    id={`price-${index}`}
+                                    className="w-16 text-right"
+                                    type="number"
+                                    value={item.price}
+                                    onChange={(e) => {
+                                        const newItems = [...items]
+                                        newItems[index].price = Number(
+                                            e.target.value
+                                        )
+                                        setItems(newItems)
+                                    }}
+                                    onKeyDown={(e) => {
+                                        if (e.key === 'Tab') return
+
+                                        const barcodeElement =
+                                            document.getElementById('barcode')
+                                        if (e.key === 'Enter') {
+                                            e.preventDefault()
+                                            barcodeElement?.focus()
+                                        }
+                                        if (e.key === 'ArrowDown') {
+                                            e.preventDefault()
+                                            const nextElement =
+                                                document.getElementById(
+                                                    `price-${index + 1}`
+                                                )
+                                            nextElement
+                                                ? nextElement.focus()
+                                                : barcodeElement?.focus()
+                                        }
+                                        if (e.key === 'ArrowUp') {
+                                            e.preventDefault()
+                                            const previousElement =
+                                                document.getElementById(
+                                                    `price-${index - 1}`
+                                                )
+                                            previousElement
+                                                ? previousElement.focus()
+                                                : null
+                                        }
+                                        if (
+                                            e.key === 'ArrowLeft' &&
+                                            !e.shiftKey
+                                        ) {
+                                            const quantityElement =
+                                                document.getElementById(
+                                                    `quantity-${index}`
+                                                )
+                                            quantityElement?.focus()
+                                        }
+                                    }}
+                                />
+                            </TableCell>
+                            <TableCell className="text-right">
+                                {item.price * item.quantity}
+                            </TableCell>
+                            <TableCell className="text-right">
+                                <Cross1Icon className="font-bold text-destructive hover:cursor-pointer" />
+                            </TableCell>
+                        </TableRow>
+                    ))}
                     <TableRow>
                         <TableCell colSpan={6} className="text-right">
                             <Input
-                                onKeyDown={(e) => {
+                                id="barcode"
+                                value={barcodeInput}
+                                onChange={(e) => {
+                                    setBarcodeInput(e.target.value)
+                                }}
+                                onKeyDown={async (e) => {
                                     if (e.key === 'Enter') {
                                         e.preventDefault()
-                                        searchSku(e.currentTarget.value)
+                                        let splitedBarcode =
+                                            e.currentTarget.value.split('*')
+                                        let barcode = splitedBarcode[0].trim()
+                                        let quantity = 1
+                                        if (splitedBarcode.length > 1) {
+                                            barcode = splitedBarcode[1].trim()
+                                            quantity = parseFloat(
+                                                splitedBarcode[0].trim()
+                                            )
+                                        }
+                                        if (!barcode.trim()) {
+                                            return
+                                        }
+                                        const isExist = items.find(
+                                            (item) => item.barcode === barcode
+                                        )
+                                        if (isExist) {
+                                            setItems(
+                                                items.map((item) =>
+                                                    item.barcode === barcode
+                                                        ? {
+                                                              ...item,
+                                                              quantity:
+                                                                  item.quantity +
+                                                                  quantity,
+                                                          }
+                                                        : item
+                                                )
+                                            )
+                                            setBarcodeInput('')
+                                            return
+                                        }
+                                        try {
+                                            const result =
+                                                await getSkuByBarcode(barcode)
+                                            setItems([
+                                                ...items,
+                                                {
+                                                    ...result,
+                                                    quantity,
+                                                    price: 0,
+                                                },
+                                            ])
+                                            setBarcodeInput('')
+                                        } catch (error) {
+                                            toast.error('ไม่พบ Barcode นี้')
+                                            return
+                                        }
+                                    }
+                                    if (e.key === 'ArrowUp') {
+                                        document
+                                            .getElementById(
+                                                `quantity-${items.length - 1}`
+                                            )
+                                            ?.focus()
                                     }
                                 }}
                             />
-                        </TableCell>
-                        {items.map((item) => (
-                            <TableRow key={item.barcode}>
-                                <TableCell>{item.barcode}</TableCell>
-                                <TableCell>{item.name}</TableCell>
-                                <TableCell className="text-right">
-                                    {item.quantity}
-                                </TableCell>
-                                <TableCell className="text-right">
-                                    {item.unit}
-                                </TableCell>
-                                <TableCell className="text-right">
-                                    {item.price}
-                                </TableCell>
-                                <TableCell className="text-right">
-                                    {item.price}
-                                </TableCell>
-                                <TableCell className="text-right">
-                                    <Cross1Icon className="font-bold text-destructive hover:cursor-pointer" />
-                                </TableCell>
-                            </TableRow>
-                        ))}
-                        <TableCell colSpan={6} className="text-right">
-                            {/* Total */}
-                            <PlusCircledIcon className="h-6 w-full text-center text-primary/50 hover:cursor-pointer hover:text-primary" />
                         </TableCell>
                     </TableRow>
                 </TableBody>
