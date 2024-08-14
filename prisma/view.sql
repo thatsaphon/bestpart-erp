0bc2dd370f41fa6d580d4f1ad5133bbc14742aad
Thatsaphon <thatphon123@hotmail.com>
2024-08-14 18:20:20 +0700

    update insertable view

--- prisma/view.sql
+++ prisma/view.sql
@@ -1,50 +1,46 @@
-CREATE VIEW StockMovement AS
+CREATE VIEW "StockMovement" AS
 SELECT
-    documentId, documentNo, date, documentType, (quantity * quantityPerUnit), cost, price
+    "documentId", "documentNo", "date", 'Sales' as "documentType", ("quantity" * "quantityPerUnit"), 0 as cost, "price" 
 FROM
-(
-SELECT
-    documentId, documentNo, date, documentType, (quantity * quantityPerUnit), 0 as cost, price 
-FROM
-    Document
+    "Document"
 INNER JOIN
-    Sales ON Document.id = Sales.documentId
+    "Sales" ON "Document".id = "Sales"."documentId"
 INNER JOIN
-    SalesItem ON Sales.id = SalesItem.salesId
+    "SalesItem" ON "Sales".id = "SalesItem"."salesId"
 UNION
 SELECT
-    documentId, documentNo, date, documentType, -(quantity * quantityPerUnit), 0 as cost, price 
+    "documentId", "documentNo", "date", 'SalesReturn' as "documentType", -("quantity" * "quantityPerUnit"), 0 as cost, "price" 
 FROM
-    Document
+    "Document"
 INNER JOIN
-    SalesReturn ON Document.id = SalesReturn.documentId
+    "SalesReturn" ON "Document".id = "SalesReturn"."documentId"
 INNER JOIN
-    SalesReturnItem ON SalesReturn.id = SalesReturnItem.salesReturnId
+    "SalesReturnItem" ON "SalesReturn".id = "SalesReturnItem"."salesReturnId"
 UNION
 SELECT
-    documentId, documentNo, date, documentType, -(quantity * quantityPerUnit), cost, 0 as price 
+    "documentId", "documentNo", "date", 'Purchase' as "documentType", -("quantity" * "quantityPerUnit"), "cost", 0 as price 
 FROM
-    Document
+    "Document"
 INNER JOIN
-    Purchase ON Document.id = Purchase.documentId
+    "Purchase" ON "Document".id = "Purchase"."documentId"
 INNER JOIN
-    PurchaseItem ON Purchase.id = PurchaseItem.purchaseId
+    "PurchaseItem" ON "Purchase".id = "PurchaseItem"."purchaseId"
 UNION
 SELECT
-    documentId, documentNo, date, documentType, (quantity * quantityPerUnit), cost, 0 as price 
+    "documentId", "documentNo", "date", 'PurchaseReturn' as "documentType", ("quantity" * "quantityPerUnit"), cost, 0 as price 
 FROM
-    Document
+    "Document"
 INNER JOIN
-    PurchaseReturn ON Document.id = PurchaseReturn.documentId
+    "PurchaseReturn" ON "Document".id = "PurchaseReturn"."documentId"
 INNER JOIN
-    PurchaseReturnItem ON PurchaseReturn.id = PurchaseReturnItem.purchaseReturnId
+    "PurchaseReturnItem" ON "PurchaseReturn".id = "PurchaseReturnItem"."purchaseReturnId"
 UNION
 SELECT
-    documentId, documentNo, date, documentType, quantity, cost, 0 as price 
+    "documentId", "documentNo", "date", 'StockAdjustment' as "documentType", "quantity", "estimatedCost" as cost, 0 as price 
 FROM
-    Document
+    "Document"
 INNER JOIN
-    StockAdjustment ON Document.id = StockAdjustment.documentId
+    "StockAdjustment" ON "Document".id = "StockAdjustment"."documentId"
 INNER JOIN
-    StockAdjustmentItem ON StockAdjustment.id = StockAdjustmentItem.stockAdjustmentId
-);
+    "StockAdjustmentItem" ON "StockAdjustment".id = "StockAdjustmentItem"."stockAdjustmentId"
+;
